bcb7cb58e9de6a94d30c0258c135049c38a29834
Graham Dennis <gdennis@anduril.com>
2025-11-24 16:07:20 +1100

    Fixup tests

--- apply-filters-without-submodules-2/test.sh
+++ apply-filters-without-submodules-2/test.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 nix flake lock --update-input example-submodules
 
 EXPECTED_NAR_HASH="sha256-J1BfVBRPqd8R8WgAebQ7sBj6gKwsbeEvwiTV7zcCazk="
-NAR_HASH="$(jq -r .nodes.example-submodules.locked.narHash flake.lock)"
+NAR_HASH="$(jq -r '.nodes."example-submodules".locked.narHash' flake.lock)"
 
 [[ "$NAR_HASH" == "$EXPECTED_NAR_HASH" ]] || echo FAIL
